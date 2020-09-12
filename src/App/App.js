febb2d089775874245bd58b351d9430e165c8f73
Nick Walters <nswalters.s@gmail.com>
2020-09-12 13:32:29 -0500

    Added left vertical navbar

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.scss';
 
+import Navbar from '../components/Navbar';
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
+        <Navbar />
+        <div className="content-area">
+          <h3>ContentArea</h3>
+        </div>
       </div>
     );
   }
