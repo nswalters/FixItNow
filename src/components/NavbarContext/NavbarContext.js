6ef1b2729423b2997d6cc1da5891c716d1b13ec3
Nick Walters <nswalters.s@gmail.com>
2020-09-12 14:46:21 -0500

    Formatted for line length

--- src/components/NavbarContext/NavbarContext.js
+++ src/components/NavbarContext/NavbarContext.js
@@ -7,7 +7,9 @@ class NavbarContext extends React.Component {
     return (
       <div className="navbar-context d-flex flex-column">
         <div className="context-header d-flex justify-content-center align-items-center">
-          <svg className="teal-600" width="31" height="31" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.5 9.667V15.5m0 5.833h.015m13.11-5.833a13.125 13.125 0 11-26.25 0 13.125 13.125 0 0126.25 0z" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/></svg>
+          <svg className="teal-600" width="31" height="31" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15.5 9.667V15.5m0 5.833h.015m13.11-5.833a13.125 13.125 0 11-26.25 0 13.125 13.125 0 0126.25 0z" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
           <h4>Incidents</h4>
         </div>
         <div className="context-items d-flex flex-column">
