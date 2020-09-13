bb117ac51e00effd75d985a2dbc7b50ed07c1ca8
Nick Walters <nswalters.s@gmail.com>
2020-09-13 13:36:30 -0500

    Added route for home page with redirect and header on home page itself

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Home.scss';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="content-header">
+        <h3 className="teal-600">Fix it Now!</h3>
+      </div>
+    );
+  }
+}
+
+export default Home;
