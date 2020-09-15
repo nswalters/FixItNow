10e6e9b2356b2aae676ada0527ff384802311629
Nick Walters <nswalters.s@gmail.com>
2020-09-14 19:36:43 -0500

    Fix line-length warning

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,9 @@ class Home extends React.Component {
         </div>
         <div className="hero-container container">
           <div className="hero mx-auto">
-            <svg className="gray-600" width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 14L14 2M2 2l12 12" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/></svg>
+            <svg className="gray-600" width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M2 14L14 2M2 2l12 12" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
             <h1>FiN - Fix it Now!</h1>
             <h4 className="mx-auto d-flex text-center align-items-center">Your single stop for incident management and service status updates.</h4>
             <div className="hero-login-button d-flex justify-content-center">
