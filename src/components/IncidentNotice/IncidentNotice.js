2a96433d50251f09faf139950ab90c0f67dd35ea
Nick Walters <nswalters.s@gmail.com>
2020-09-15 20:36:11 -0500

    Initial component creation

---
+++ src/components/IncidentNotice/IncidentNotice.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class IncidentNotice extends Component {
+  render() {
+    return (
+      <div>
+        <h2 className="text-center">INCIDENT NOTICE</h2>
+      </div>
+    );
+  }
+}
+
+export default IncidentNotice;
