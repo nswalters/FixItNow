e9fae60e62940f79489e51ba2637a2d81a977006
Nick Walters <nswalters.s@gmail.com>
2020-09-18 18:31:43 -0500

    Add functions to get all services and get user services by user id

--- src/helpers/data/services/servicesData.js
+++ src/helpers/data/services/servicesData.js
@@ -22,4 +22,39 @@ const getAllPublicServices = () => new Promise((resolve, reject) => {
     .catch((err) => reject(err));
 });
 
-export default { getAllPublicServices };
+const getAllServices = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/service.json`)
+    .then((response) => {
+      const serviceObjects = response.data;
+      const services = [];
+
+      if (serviceObjects) {
+        Object.keys(serviceObjects).forEach((serviceId) => {
+          serviceObjects[serviceId].id = serviceId;
+          services.push(serviceObjects[serviceId]);
+        });
+      }
+      resolve(services);
+    })
+    .catch((err) => reject(err));
+});
+
+const getUserServicesByUid = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/service_user.json?orderBy="user_id"&equalTo="${uid}"`)
+    .then((response) => {
+      const serviceUserObjects = response.data;
+      const userServices = [];
+
+      getAllServices().then((allServices) => {
+        Object.keys(serviceUserObjects).forEach((serviceUser) => {
+          const userSvc = allServices.find((i) => i.id === serviceUserObjects[serviceUser].service_id);
+          userServices.push(userSvc);
+        });
+
+        resolve(userServices);
+      });
+    })
+    .catch((err) => reject(err));
+});
+
+export default { getAllServices, getAllPublicServices, getUserServicesByUid };
