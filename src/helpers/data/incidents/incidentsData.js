f15650bab7cf158b04d56f1bfd765d8a80e50076
Nick Walters <nswalters.s@gmail.com>
2020-09-17 22:29:35 -0500

    Added functions to get user incidents by user id;Added function to get all incidents;Fixed variable naming error

--- src/helpers/data/incidents/incidentsData.js
+++ src/helpers/data/incidents/incidentsData.js
@@ -4,6 +4,23 @@ import apiKeys from '../../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseConfig.databaseURL;
 
+const getAllIncidents = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/incident.json`)
+    .then((response) => {
+      const incidentObjects = response.data;
+      const incidents = [];
+
+      if (incidentObjects) {
+        Object.keys(incidentObjects).forEach((incidentId) => {
+          incidentObjects[incidentId].id = incidentId;
+          incidents.push(incidentObjects[incidentId]);
+        });
+      }
+      resolve(incidents);
+    })
+    .catch((err) => reject(err));
+});
+
 const getAllPublicIncidents = () => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/incident.json?orderBy="is_public"&equalTo=true`)
     .then((response) => {
@@ -11,10 +28,10 @@ const getAllPublicIncidents = () => new Promise((resolve, reject) => {
       const pubIncidents = [];
 
       if (allIncidents) {
-        Object.keys(allIncidents).forEach((serviceId) => {
-          const service = allIncidents[serviceId];
-          service.id = serviceId;
-          pubIncidents.push(service);
+        Object.keys(allIncidents).forEach((incidentId) => {
+          const incident = allIncidents[incidentId];
+          incident.id = incidentId;
+          pubIncidents.push(incident);
         });
       }
       resolve(pubIncidents);
@@ -22,4 +39,22 @@ const getAllPublicIncidents = () => new Promise((resolve, reject) => {
     .catch((err) => reject(err));
 });
 
-export default { getAllPublicIncidents };
+const getUserIncidentsByUid = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/incident_user.json?orderBy="user_id"&equalTo="${uid}"`)
+    .then((response) => {
+      const incidentUserObjects = response.data;
+      const userIncidents = [];
+
+      getAllIncidents().then((allIncidents) => {
+        Object.keys(incidentUserObjects).forEach((incidentUser) => {
+          const userInc = allIncidents.find((i) => i.id === incidentUserObjects[incidentUser].incident_id);
+          userIncidents.push(userInc);
+        });
+
+        resolve(userIncidents);
+      });
+    })
+    .catch((err) => reject(err));
+});
+
+export default { getAllIncidents, getAllPublicIncidents, getUserIncidentsByUid };
