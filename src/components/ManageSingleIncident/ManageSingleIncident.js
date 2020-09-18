38d2b3a758ad101019bd6d978450a797aea13e00
Nick Walters <nswalters.s@gmail.com>
2020-09-18 18:32:41 -0500

    Use lookup tables from session storage to populate incident values

--- src/components/ManageSingleIncident/ManageSingleIncident.js
+++ src/components/ManageSingleIncident/ManageSingleIncident.js
@@ -1,23 +1,31 @@
 /* eslint-disable max-len */
 import React, { Component } from 'react';
 
+import lookupData from '../../helpers/data/lookupData/lookupData';
+
 import './ManageSingleIncident.scss';
 
 class ManageSingleIncident extends Component {
   render() {
+    const { incident } = this.props;
+
+    // const lu_impact = lookupData.readLookupImpact(incident.impact_id);
+    const luStatusType = lookupData.readLookupStatusType(incident.status_type_id);
+    const luSeverity = lookupData.readLookupSeverity(incident.severity_id);
+
     return (
       <div className="incident-details">
         <div className="top-row d-flex flex-row">
-          <div className="severity-badge d-flex justify-content-center mt-4" style={{ backgroundColor: '#ED8936' }}>
-            <span className="severity-badge-text">P2</span>
+          <div className="severity-badge d-flex justify-content-center mt-4" style={{ backgroundColor: luSeverity.color }}>
+            <span className="severity-badge-text">{ luSeverity.name }</span>
           </div>
-          <div className="incident-details-name">Increased Volume of HTTP 500 Errors</div>
+        <div className="incident-details-name">{ incident.title }</div>
           <div className="view-badge ml-auto mt-4 d-flex justify-content-center align-items-center">
-            <span className="view-badge-text mx-auto">Public</span>
+            <span className="view-badge-text mx-auto">{ incident.is_public ? 'Public' : 'Private' }</span>
           </div>
         </div>
         <div className="bottom-row d-flex flex-row">
-          <span className="incident-status-badge text-center py-auto my-auto" style={{ backgroundColor: '#FC8181' }}>Investigating</span>
+          <span className="incident-status-badge text-center py-auto my-auto" style={{ backgroundColor: luStatusType.color }}>{ luStatusType.name }</span>
           <div className="incident-owners d-flex flex-row">
             <div className="individual-owner d-flex flex-row">
               <svg className="owner-icon my-auto" width="15" height="15" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M7.125 6a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0h-14z" fill="#1A202C"/></svg>
