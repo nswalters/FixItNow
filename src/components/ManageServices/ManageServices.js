80caf2577cebeada9b421a2426d3977253274488
Nick Walters <nswalters.s@gmail.com>
2020-09-18 18:33:51 -0500

    Dynamically populate manage page with service components and respective data

--- src/components/ManageServices/ManageServices.js
+++ src/components/ManageServices/ManageServices.js
@@ -1,11 +1,43 @@
 import React from 'react';
 
-class Services extends React.Component {
+import ManageSingleService from '../ManageSingleService/ManageSingleService';
+
+import servicesData from '../../helpers/data/services/servicesData';
+
+import './ManageServices.scss';
+
+class ManageServices extends React.Component {
+  state = {
+    userServices: [],
+  }
+
+  componentDidMount() {
+    const { uid } = this.props;
+
+    servicesData.getUserServicesByUid(uid)
+      .then((userServices) => {
+        this.setState({ userServices });
+      })
+      .catch((err) => console.error('Could not get services by uid: ', err));
+  }
+
   render() {
+    const { userServices } = this.state;
+
+    const allUserServices = userServices.map((service) => (
+      <ManageSingleService key={service.id} service={service} />));
+
     return (
-      <h1>Services</h1>
+      <div className="content">
+        <div className="content-header">
+          <h3>Manage Services</h3>
+        </div>
+        <div className="service-list-container container">
+          { allUserServices }
+        </div>
+      </div>
     );
   }
 }
 
-export default Services;
+export default ManageServices;
